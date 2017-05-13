e5e76af4ac46b3573dfcddea6ce21afea3efe118
andy1n9 <ykia1234@hotmail.com>
2017-05-12 23:12:10 -0700

    update

--- run_servers.sh
+++ run_servers.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-steamcmd_dir="$HOME/steamcmd"
 install_dir="$HOME/dontstarvetogether_dedicated_server"
 cluster_name="MyDediServer"
 dontstarve_dir="$HOME/.klei/DoNotStarveTogether"
@@ -18,15 +17,13 @@ function check_for_file()
     fi
 }
 
-cd "$steamcmd_dir" || fail "Missing $steamcmd_dir directory!"
 
-check_for_file "steamcmd.sh"
 check_for_file "$dontstarve_dir/$cluster_name/cluster.ini"
 check_for_file "$dontstarve_dir/$cluster_name/cluster_token.txt"
 check_for_file "$dontstarve_dir/$cluster_name/Master/server.ini"
 check_for_file "$dontstarve_dir/$cluster_name/Caves/server.ini"
 
-./steamcmd.sh +force_install_dir "$install_dir" +login ykia1234 Andy5201314 +app_update 343050 validate +quit
+steamcmd +force_install_dir "$install_dir" +login ykia1234 Andy5201314 +app_update 343050 validate +quit
 
 check_for_file "$install_dir/bin"
 
