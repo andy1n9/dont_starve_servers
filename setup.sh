4e74152a7708292b42c68ad53010e8f081b3d895
andy1n9 <ykia1234@hotmail.com>
2017-05-13 14:05:09 -0700

    update_script

--- setup.sh
+++ setup.sh
@@ -4,3 +4,33 @@ dontstarve_dir="$HOME/.klei"
 
 mkdir -p $dontstarve_dir
 cp -r DoNotStarveTogether $dontstarve_dir
+
+steamcmd_dir="$HOME/steamcmd"
+install_dir="$HOME/dontstarvetogether_dedicated_server"
+cluster_name="MyDediServer"
+dontstarve_dir="$HOME/.klei/DoNotStarveTogether"
+
+function fail()
+{
+        echo Error: "$@" >&2
+        exit 1
+}
+
+function check_for_file()
+{
+    if [ ! -e "$1" ]; then
+            fail "Missing file: $1"
+    fi
+}
+
+cd "$steamcmd_dir" || fail "Missing $steamcmd_dir directory!"
+
+check_for_file "steamcmd.sh"
+check_for_file "$dontstarve_dir/$cluster_name/cluster.ini"
+check_for_file "$dontstarve_dir/$cluster_name/cluster_token.txt"
+check_for_file "$dontstarve_dir/$cluster_name/Master/server.ini"
+check_for_file "$dontstarve_dir/$cluster_name/Caves/server.ini"
+
+./steamcmd.sh +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
+
+cp dedicated_server_mods_setup.lua $install_dir/mods/
