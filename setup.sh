4b35d9cbcc5627b122db15cb0cc347c080b96e5d
andy1n9 <ykia1234@hotmail.com>
2017-05-12 21:50:57 -0700

    upload_server

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+steamcmd_dir="$HOME/steamcmd"
+install_dir="$HOME/dontstarvetogether_dedicated_server"
+cluster_name="MyDediServer"
+dontstarve_dir="$HOME/.klei/DoNotStarveTogether"
+
+mkdir -p $(dontstarve_dir)
+mv DoNotStarveTogether $(dontstarve_dir)
